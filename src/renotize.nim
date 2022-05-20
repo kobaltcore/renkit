6ae338b49659d41fd13cded76c1e2dcb8a7bb2c5
cobaltcore <cobaltcore@yandex.com>
2022-05-20 23:15:54 +0200

    Fix renotize

--- src/renotize.nim
+++ src/renotize.nim
@@ -5,8 +5,10 @@ import osproc
 import cligen
 import options
 import strutils
+import sequtils
 import strformat
 import parsetoml
+import zippy/internal
 import zippy/ziparchives
 
 type KeyboardInterrupt = object of CatchableError
@@ -21,16 +23,43 @@ proc unpack_app*(input_file: string, output_dir = "") =
   if target_dir != "" and dirExists(target_dir):
     removeDir(target_dir)
   if target_dir == "":
-    target_dir = splitPath(input_file)[0]
+    target_dir = input_file
+    removeSuffix(target_dir, ".zip")
+
   extractAll(input_file, target_dir)
+
+  let extracted_file = walkDirs(joinPath(target_dir, "*.app")).toSeq()[0]
+  let new_target_dir = joinPath(splitPath(target_dir)[0], splitPath(extracted_file)[1])
+  moveFile(extracted_file, new_target_dir)
+  removeDir(target_dir)
 
 proc sign_app*(input_file: string, identity: string) =
   let entitlements = """<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"><plist version="1.0"><dict><key>com.apple.security.cs.allow-unsigned-executable-memory</key><true/></dict></plist>"""
+
+  writeFile("entitlements.plist", entitlements)
 
   let cmd = &"codesign --entitlements=entitlements.plist --options=runtime --timestamp -s '{identity}' -f --deep --no-strict {input_file}"
   discard execShellCmd(cmd)
 
   removeFile("entitlements.plist")
+
+proc addDir(archive: ZipArchive, base, relative: string) =
+  if relative.len > 0 and relative notin archive.contents:
+    archive.contents[(relative & os.DirSep).toUnixPath()] =
+      ArchiveEntry(kind: ekDirectory)
+
+  for kind, path in walkDir(base / relative, relative = true):
+    case kind:
+    of pcFile:
+      archive.contents[(relative / path).toUnixPath()] = ArchiveEntry(
+        kind: ekFile,
+        contents: readFile(base / relative / path),
+        lastModified: getLastModificationTime(base / relative / path),
+      )
+    of pcDir:
+      archive.addDir(base, relative / path)
+    else:
+      discard
 
 proc notarize_app*(
   input_file: string,
@@ -43,18 +72,21 @@ proc notarize_app*(
   removeSuffix(app_zip, ".app")
   app_zip = &"{app_zip}-app.zip"
 
-  createZipArchive(input_file, app_zip)
+  let archive = ZipArchive()
+  let (head, tail) = splitPath(input_file)
+  archive.addDir(head, tail)
+  archive.writeZipArchive(app_zip)
 
   let cmd = &"xcrun altool {altool_extra} -u {apple_id} -p {password} --notarize-app --primary-bundle-id {bundle_id} -f {app_zip}"
   let output = execProcess(cmd)
-  echo(output)
+  echo output
 
   let match = find(output, re"RequestUUID = ([A-z0-9-]+)")
   if match.isNone:
-    echo("Could not retrieve UUID")
+    echo "Could not retrieve UUID"
     quit(1)
 
-  let uuid = match.get.captures[1]
+  let uuid = match.get.captures[0]
 
   return uuid
 
@@ -81,18 +113,16 @@ proc notarize_dmg*(
   password: string,
   altool_extra = "",
 ): string =
-  # zip input, then notarize that zip file
-
   let cmd = &"xcrun altool {altool_extra} -u {apple_id} -p {password} --notarize-app --primary-bundle-id {bundle_id} -f {input_file}"
   let output = execProcess(cmd)
-  echo(output)
+  echo output
 
   let match = find(output, re"RequestUUID = ([A-z0-9-]+)")
   if match.isNone:
-    echo("Could not retrieve UUID")
+    echo "Could not retrieve UUID"
     quit(1)
 
-  let uuid = match.get.captures[1]
+  let uuid = match.get.captures[0]
 
   return uuid
 
@@ -119,7 +149,6 @@ proc full_run*(input_file: string, config: string) =
   var
     uuid: string
     status: string
-    base_name: string
 
   let config = parsetoml.parseFile(config)
   let altool_extra = config["altool_extra"].getStr()
@@ -128,42 +157,57 @@ proc full_run*(input_file: string, config: string) =
   let apple_id = config["apple_id"].getStr()
   let password = config["password"].getStr()
 
+  echo "Unpacking app"
   unpack_app(input_file)
 
-  base_name = input_file
-  removeSuffix(base_name, ".zip")
+  let app_file = walkDirs(joinPath(splitPath(input_file)[0], "*.app")).toSeq()[0]
 
-  let app_file = &"{base_name}.app"
-  let dmg_file = &"{base_name}.dmg"
-
-  sign_app(&"{base_name}.app", identity)
-
-  uuid = notarize_app(app_file, bundle_id, apple_id, password, altool_extra)
-
-  status = status(uuid, apple_id, password, altool_extra)
-  while status != "success" or status != "invalid":
-    status = status(uuid, apple_id, password, altool_extra)
-    sleep(10_000)
-
-  staple_app(app_file)
-
+  echo "Signing app"
   sign_app(app_file, identity)
 
-  pack_dmg(app_file, dmg_file, base_name)
+  echo "Notarizing app"
+  uuid = notarize_app(app_file, bundle_id, apple_id, password, altool_extra)
 
-  sign_dmg(dmg_file, identity)
-
-  uuid = notarize_dmg(dmg_file, bundle_id, apple_id, password, altool_extra)
-
+  echo "Waiting for notarization"
   status = status(uuid, apple_id, password, altool_extra)
-  while status != "success" or status != "invalid":
+  while status != "success" and status != "invalid":
+    echo "."
     status = status(uuid, apple_id, password, altool_extra)
     sleep(10_000)
 
+  echo "Stapling app"
+  staple_app(app_file)
+
+  echo "Signing stapled app"
+  sign_app(app_file, identity)
+
+  let (dir, name, ext) = splitFile(app_file)
+  let dmg_file = &"{joinPath(dir, name)}.dmg"
+
+  echo "Packing DMG"
+  pack_dmg(app_file, dmg_file, input_file)
+
+  echo "Signing DMG"
+  sign_dmg(dmg_file, identity)
+
+  echo "Notarizing DMG"
+  uuid = notarize_dmg(dmg_file, bundle_id, apple_id, password, altool_extra)
+
+  echo "Waiting for notarization"
+  status = status(uuid, apple_id, password, altool_extra)
+  while status != "success" and status != "invalid":
+    echo "."
+    status = status(uuid, apple_id, password, altool_extra)
+    sleep(10_000)
+
+  echo "Stapling DMG"
   staple_dmg(dmg_file)
 
+  echo "Cleaning up"
   removeFile("*-app.zip")
   removeDir("extracted distro")
+
+  echo "Done"
 
 when isMainModule:
   dispatchMulti(
