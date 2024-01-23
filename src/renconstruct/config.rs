9bcad59263cb2cb97e491345950c3ff4fa7dff90
cobaltcore <cobaltcore@yandex.com>
2024-01-23 15:03:35 +0100

    Add more missing files

---
+++ src/renconstruct/config.rs
@@ -0,0 +1,147 @@
+use crate::common::Version;
+use serde::{Deserialize, Deserializer};
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+};
+
+fn deserialize_version<'de, D>(deserializer: D) -> Result<Version, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let buf = String::deserialize(deserializer)?;
+    match Version::from_str(&buf) {
+        Some(version) => Ok(version),
+        None => Err(serde::de::Error::custom(format!(
+            "Invalid version: {}",
+            buf
+        ))),
+    }
+}
+
+fn default_as_true() -> bool {
+    true
+}
+
+fn default_convert_images_extensions() -> Vec<String> {
+    vec!["png".into(), "jpg".into(), "jpeg".into()]
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub enum ImageFormat {
+    #[serde(alias = "webp")]
+    WebP,
+    #[serde(alias = "avif")]
+    Avif,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ConvertImagesPathConfig {
+    #[serde(default = "default_as_true")]
+    pub recursive: bool,
+    #[serde(default = "default_as_true")]
+    pub lossless: bool,
+    #[serde(default = "default_convert_images_extensions")]
+    pub extensions: Vec<String>,
+}
+
+#[derive(Debug, Clone, Deserialize, Default)]
+pub struct PriorityOptions {
+    #[serde(default)]
+    pub pre_build: usize,
+    #[serde(default)]
+    pub post_build: usize,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct KeystoreOptions {
+    pub keystore_apk: String,
+    pub keystore_aab: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct ConvertImagesOptions {
+    pub format: ImageFormat,
+    pub paths: HashMap<String, ConvertImagesPathConfig>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct NotarizeOptions {
+    pub bundle_identifier: String,
+    pub json_bundle_file: String,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct CustomOptions {
+    pub options: HashMap<String, String>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(tag = "type")]
+pub enum TaskOptions {
+    #[serde(rename = "keystore")]
+    Keystore(KeystoreOptions),
+    #[serde(rename = "convert_images")]
+    ConvertImages(ConvertImagesOptions),
+    #[serde(rename = "notarize")]
+    Notarize(NotarizeOptions),
+    #[serde(rename = "custom")]
+    Custom(CustomOptions),
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(tag = "type")]
+pub struct GeneralTaskOptions {
+    pub enabled: bool,
+    #[serde(default)]
+    pub on_builds: HashSet<String>,
+    #[serde(default)]
+    pub priorities: PriorityOptions,
+    #[serde(flatten)]
+    pub options: TaskOptions,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub build: BuildOptions,
+    pub options: RenconstructOptions,
+    pub renutil: RenutilOptions,
+    pub tasks: HashMap<String, GeneralTaskOptions>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RenconstructOptions {
+    pub task_dir: Option<PathBuf>,
+    pub clear_output_dir: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RenutilOptions {
+    #[serde(deserialize_with = "deserialize_version")]
+    pub version: Version,
+    pub registry: Option<PathBuf>,
+    #[serde(default)]
+    pub update_pickle: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BuildOptions {
+    #[serde(default)]
+    pub pc: bool,
+    #[serde(default)]
+    pub win: bool,
+    #[serde(default)]
+    pub linux: bool,
+    #[serde(default)]
+    pub mac: bool,
+    #[serde(default)]
+    pub web: bool,
+    #[serde(default)]
+    pub steam: bool,
+    #[serde(default)]
+    pub market: bool,
+    #[serde(default)]
+    pub android_apk: bool,
+    #[serde(default)]
+    pub android_aab: bool,
+}
---
+++ src/renconstruct/tasks.rs
@@ -0,0 +1,388 @@
+use super::config::{
+    ConvertImagesOptions, GeneralTaskOptions, ImageFormat, KeystoreOptions, NotarizeOptions,
+};
+use crate::common::Version;
+use anyhow::Result;
+use base64::prelude::*;
+use command_executor::command::Command;
+use command_executor::shutdown_mode::ShutdownMode;
+use command_executor::thread_pool_builder::ThreadPoolBuilder;
+use imgref::ImgRef;
+use indicatif::{ProgressBar, ProgressStyle};
+use jwalk::WalkDir;
+use ravif::Encoder;
+use rgb::FromSlice;
+use std::io::Read;
+use std::path::PathBuf;
+use std::{env, fs};
+use {anyhow::anyhow, image::io::Reader as ImageReader, image::EncodableLayout};
+
+#[derive(Debug)]
+pub struct Task {
+    pub name: String,
+    pub kind: GeneralTaskOptions,
+    // TODO: add some kind of handle to call custom tasks here
+}
+
+#[derive(Debug, Clone)]
+pub struct TaskContext {
+    pub version: Version,
+    pub input_dir: PathBuf,
+    pub output_dir: PathBuf,
+    pub renpy_path: PathBuf,
+}
+
+pub struct ProcessingCommand {
+    pub image_format: ImageFormat,
+    pub path: PathBuf,
+    pub lossless: bool,
+}
+
+impl ProcessingCommand {
+    fn new(image_format: ImageFormat, path: PathBuf, lossless: bool) -> ProcessingCommand {
+        ProcessingCommand {
+            image_format,
+            path,
+            lossless,
+        }
+    }
+}
+
+impl Command for ProcessingCommand {
+    fn execute(&self) -> Result<()> {
+        match self.image_format {
+            ImageFormat::Avif => {
+                let image = ImageReader::open(&self.path)?.decode()?;
+                let image = image.to_rgba8();
+                let image = ImgRef::new(
+                    image.as_rgba(),
+                    image.width() as usize,
+                    image.height() as usize,
+                );
+
+                let avif_enc = Encoder::new()
+                    .with_quality(90.0)
+                    .with_speed(5)
+                    .with_num_threads(Some(1));
+                let img = avif_enc.encode_rgba(image)?;
+
+                fs::write(&self.path, &img.avif_file)?;
+            }
+            ImageFormat::WebP => {
+                let image = ImageReader::open(&self.path)?.decode()?;
+                let image = image.to_rgba8();
+
+                let enc = webp::Encoder::from_rgba(&image, image.width(), image.height());
+
+                let result = match self.lossless {
+                    true => {
+                        // -lossless -z 9 -m 6
+                        enc.encode_advanced(&webp::WebPConfig {
+                            lossless: 1,
+                            quality: 100.0,
+                            method: 6,
+                            image_hint: libwebp_sys::WebPImageHint::WEBP_HINT_PICTURE,
+                            target_size: 0,
+                            target_PSNR: 0.0,
+                            segments: 4,
+                            sns_strength: 50,
+                            filter_strength: 60,
+                            filter_sharpness: 0,
+                            filter_type: 0,
+                            autofilter: 0,
+                            alpha_compression: 1,
+                            alpha_filtering: 1,
+                            alpha_quality: 100,
+                            pass: 5,
+                            show_compressed: 0,
+                            preprocessing: 0,
+                            partitions: 0,
+                            partition_limit: 0,
+                            emulate_jpeg_size: 0,
+                            thread_level: 0,
+                            low_memory: 0,
+                            near_lossless: 0,
+                            exact: 0,
+                            use_delta_palette: 0,
+                            use_sharp_yuv: 1,
+                            qmin: 0,
+                            qmax: 100,
+                        })
+                        .map_err(|err| anyhow!("Error encoding WebP image: {:?}", err))?
+                    }
+                    false => {
+                        // -q 90 -m 6 -sharp_yuv -pre 4
+                        enc.encode_advanced(&webp::WebPConfig {
+                            lossless: 1,
+                            quality: 90.0,
+                            method: 6,
+                            image_hint: libwebp_sys::WebPImageHint::WEBP_HINT_PICTURE,
+                            target_size: 0,
+                            target_PSNR: 0.0,
+                            segments: 4,
+                            sns_strength: 50,
+                            filter_strength: 60,
+                            filter_sharpness: 0,
+                            filter_type: 0,
+                            autofilter: 0,
+                            alpha_compression: 1,
+                            alpha_filtering: 1,
+                            alpha_quality: 100,
+                            pass: 5,
+                            show_compressed: 0,
+                            preprocessing: 4,
+                            partitions: 0,
+                            partition_limit: 0,
+                            emulate_jpeg_size: 0,
+                            thread_level: 0,
+                            low_memory: 0,
+                            near_lossless: 0,
+                            exact: 0,
+                            use_delta_palette: 0,
+                            use_sharp_yuv: 1,
+                            qmin: 0,
+                            qmax: 90,
+                        })
+                        .map_err(|err| anyhow!("Error encoding WebP image: {:?}", err))?
+                    }
+                };
+
+                fs::write(&self.path, &result.as_bytes())?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/*
+fn load_rgba(data: &[u8], premultiplied_alpha: bool) -> Result<ImgVec<RGBA8>> {
+    let img = load_image::load_data(data)?.into_imgvec();
+    let mut img = match img {
+        load_image::export::imgref::ImgVecKind::RGB8(img) => {
+            img.map_buf(|buf| buf.into_iter().map(|px| px.alpha(255)).collect())
+        }
+        load_image::export::imgref::ImgVecKind::RGBA8(img) => img,
+        load_image::export::imgref::ImgVecKind::RGB16(img) => img.map_buf(|buf| {
+            buf.into_iter()
+                .map(|px| px.map(|c| (c >> 8) as u8).alpha(255))
+                .collect()
+        }),
+        load_image::export::imgref::ImgVecKind::RGBA16(img) => img.map_buf(|buf| {
+            buf.into_iter()
+                .map(|px| px.map(|c| (c >> 8) as u8))
+                .collect()
+        }),
+        load_image::export::imgref::ImgVecKind::GRAY8(img) => img.map_buf(|buf| {
+            buf.into_iter()
+                .map(|g| {
+                    let c = g.0;
+                    RGBA8::new(c, c, c, 255)
+                })
+                .collect()
+        }),
+        load_image::export::imgref::ImgVecKind::GRAY16(img) => img.map_buf(|buf| {
+            buf.into_iter()
+                .map(|g| {
+                    let c = (g.0 >> 8) as u8;
+                    RGBA8::new(c, c, c, 255)
+                })
+                .collect()
+        }),
+        load_image::export::imgref::ImgVecKind::GRAYA8(img) => img.map_buf(|buf| {
+            buf.into_iter()
+                .map(|g| {
+                    let c = g.0;
+                    RGBA8::new(c, c, c, g.1)
+                })
+                .collect()
+        }),
+        load_image::export::imgref::ImgVecKind::GRAYA16(img) => img.map_buf(|buf| {
+            buf.into_iter()
+                .map(|g| {
+                    let c = (g.0 >> 8) as u8;
+                    RGBA8::new(c, c, c, (g.1 >> 8) as u8)
+                })
+                .collect()
+        }),
+    };
+
+    if premultiplied_alpha {
+        img.pixels_mut().for_each(|px| {
+            px.r = (u16::from(px.r) * u16::from(px.a) / 255) as u8;
+            px.g = (u16::from(px.g) * u16::from(px.a) / 255) as u8;
+            px.b = (u16::from(px.b) * u16::from(px.a) / 255) as u8;
+        });
+    }
+    Ok(img)
+}
+*/
+
+pub fn task_keystore_pre(ctx: &TaskContext, options: &KeystoreOptions) -> Result<()> {
+    let android_path;
+    let android_path_backup;
+    let bundle_path;
+    let bundle_path_backup;
+
+    if ctx.version < Version::from_str("7.6.0").unwrap()
+        && ctx.version < Version::from_str("8.1.0").unwrap()
+    {
+        android_path = ctx.renpy_path.join("rapt/android.keystore");
+        android_path_backup = ctx.renpy_path.join("rapt/android.keystore.original");
+        bundle_path = ctx.renpy_path.join("rapt/bundle.keystore");
+        bundle_path_backup = ctx.renpy_path.join("rapt/bundle.keystore.original");
+    } else {
+        android_path = ctx.input_dir.join("android.keystore");
+        android_path_backup = ctx.input_dir.join("android.keystore.original");
+        bundle_path = ctx.input_dir.join("bundle.keystore");
+        bundle_path_backup = ctx.input_dir.join("bundle.keystore.original");
+    };
+
+    if android_path.exists() && !android_path_backup.exists() {
+        fs::copy(&android_path, &android_path_backup)?;
+    }
+
+    if bundle_path.exists() && !bundle_path_backup.exists() {
+        fs::copy(&bundle_path, &bundle_path_backup)?;
+    }
+
+    let android_keystore = match env::var("RC_KEYSTORE_APK") {
+        Ok(val) => BASE64_STANDARD.decode(val)?,
+        Err(_) => BASE64_STANDARD.decode(options.keystore_apk.clone())?,
+    };
+
+    fs::write(&android_path, android_keystore)?;
+
+    let bundle_keystore = match env::var("RC_KEYSTORE_AAB") {
+        Ok(val) => BASE64_STANDARD.decode(val)?,
+        Err(_) => BASE64_STANDARD.decode(options.keystore_aab.clone())?,
+    };
+    fs::write(&bundle_path, bundle_keystore)?;
+
+    Ok(())
+}
+
+pub fn task_keystore_post(ctx: &TaskContext, _options: &KeystoreOptions) -> Result<()> {
+    let android_path;
+    let android_path_backup;
+    let bundle_path;
+    let bundle_path_backup;
+
+    if ctx.version < Version::from_str("7.6.0").unwrap()
+        && ctx.version < Version::from_str("8.1.0").unwrap()
+    {
+        android_path = ctx.renpy_path.join("rapt/android.keystore");
+        android_path_backup = ctx.renpy_path.join("rapt/android.keystore.original");
+        bundle_path = ctx.renpy_path.join("rapt/bundle.keystore");
+        bundle_path_backup = ctx.renpy_path.join("rapt/bundle.keystore.original");
+    } else {
+        android_path = ctx.input_dir.join("android.keystore");
+        android_path_backup = ctx.input_dir.join("android.keystore.original");
+        bundle_path = ctx.input_dir.join("bundle.keystore");
+        bundle_path_backup = ctx.input_dir.join("bundle.keystore.original");
+    };
+
+    if android_path_backup.exists() {
+        fs::copy(&android_path_backup, &android_path)?;
+        fs::remove_file(&android_path_backup)?;
+    } else {
+        fs::remove_file(&android_path)?;
+    }
+
+    if bundle_path_backup.exists() {
+        fs::copy(&bundle_path_backup, &bundle_path)?;
+        fs::remove_file(&bundle_path_backup)?;
+    } else {
+        fs::remove_file(&bundle_path)?;
+    }
+
+    Ok(())
+}
+
+pub fn task_convert_images_pre(ctx: &TaskContext, options: &ConvertImagesOptions) -> Result<()> {
+    let mut files = vec![];
+
+    for (path, opts) in &options.paths {
+        let path = ctx.input_dir.join(path);
+        if !path.exists() {
+            println!("Path does not exist: {}", path.display());
+            continue;
+        }
+
+        for entry in match opts.recursive {
+            true => WalkDir::new(path),
+            false => WalkDir::new(path).max_depth(1),
+        } {
+            match entry {
+                Ok(entry) => {
+                    if entry.path().is_dir() {
+                        continue;
+                    }
+                    match entry.path().extension() {
+                        Some(ext) => {
+                            if !opts.extensions.contains(&ext.to_string_lossy().to_string()) {
+                                continue;
+                            }
+                            // read first 16 bytes
+                            let mut buf = [0; 12];
+                            let mut file = fs::File::open(entry.path())?;
+                            file.read_exact(&mut buf)?;
+                            drop(file);
+                            if String::from_utf8_lossy(&buf[0..4]).to_string().as_str() == "RIFF" {
+                                continue;
+                            }
+                            if String::from_utf8_lossy(&buf[4..12]).to_string().as_str()
+                                == "ftypavif"
+                            {
+                                continue;
+                            }
+                            files.push((entry.path(), opts.lossless));
+                        }
+                        None => continue,
+                    }
+                }
+                Err(err) => {
+                    println!("Error: {}", err);
+                    continue;
+                }
+            }
+        }
+    }
+
+    let bar =ProgressBar::new(files.len() as u64).with_style(
+        ProgressStyle::with_template(
+            "{bar:48.green/black} {human_pos:>5.green}/{human_len:<5.green} {per_sec:.red} eta {eta:.blue}",
+        )
+        .unwrap()
+        .progress_chars("━╾╴─"),
+    );
+
+    let num_cpus = (num_cpus::get() - 1).max(1);
+
+    let mut pool = ThreadPoolBuilder::new()
+        .with_name_str("pool-name")
+        .with_tasks(num_cpus)
+        .with_queue_size(num_cpus * 2)
+        .with_shutdown_mode(ShutdownMode::CompletePending)
+        .build()?;
+
+    for (path, lossless) in files {
+        pool.submit(Box::new(ProcessingCommand::new(
+            options.format.clone(),
+            path,
+            lossless,
+        )));
+        bar.inc(1);
+    }
+
+    pool.shutdown();
+    pool.join()?;
+
+    bar.finish();
+
+    Ok(())
+}
+
+pub fn task_notarize_post(ctx: &TaskContext, options: &NotarizeOptions) -> Result<()> {
+    Ok(())
+}
